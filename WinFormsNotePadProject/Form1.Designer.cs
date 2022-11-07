61ac00686cb6b52d26f68338cb146595eb7b9b73
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-07 16:05:03 +0300

    07-11-22

--- WinFormsNotePadProject/Form1.Designer.cs
+++ WinFormsNotePadProject/Form1.Designer.cs
@@ -28,7 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtEditBox = new System.Windows.Forms.TextBox();
+            this.cntMenuEditBox = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cntMenuEditCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.cntMenuEditPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.cntMenuEditCut = new System.Windows.Forms.ToolStripMenuItem();
             this.dialogOpen = new System.Windows.Forms.OpenFileDialog();
             this.dialogSave = new System.Windows.Forms.SaveFileDialog();
             this.dialogFont = new System.Windows.Forms.FontDialog();
@@ -42,17 +48,61 @@
             this.menuItemFileClose = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.menuItemAppExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolMain = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.statusMain = new System.Windows.Forms.StatusStrip();
+            this.stLabelRowTitle = new System.Windows.Forms.ToolStripStatusLabel();
+            this.stLabelRow = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cntMenuEditBox.SuspendLayout();
             this.menuMain.SuspendLayout();
+            this.toolMain.SuspendLayout();
+            this.statusMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtEditBox
             // 
-            this.txtEditBox.Location = new System.Drawing.Point(21, 55);
+            this.txtEditBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEditBox.ContextMenuStrip = this.cntMenuEditBox;
+            this.txtEditBox.Location = new System.Drawing.Point(0, 52);
             this.txtEditBox.Multiline = true;
             this.txtEditBox.Name = "txtEditBox";
             this.txtEditBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtEditBox.Size = new System.Drawing.Size(741, 491);
+            this.txtEditBox.Size = new System.Drawing.Size(791, 512);
             this.txtEditBox.TabIndex = 1;
+            this.txtEditBox.TextChanged += new System.EventHandler(this.txtEditBox_TextChanged);
+            this.txtEditBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEditBox_KeyDown);
+            // 
+            // cntMenuEditBox
+            // 
+            this.cntMenuEditBox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cntMenuEditCopy,
+            this.cntMenuEditPaste,
+            this.cntMenuEditCut});
+            this.cntMenuEditBox.Name = "cntMenuEditBox";
+            this.cntMenuEditBox.Size = new System.Drawing.Size(181, 92);
+            // 
+            // cntMenuEditCopy
+            // 
+            this.cntMenuEditCopy.Name = "cntMenuEditCopy";
+            this.cntMenuEditCopy.Size = new System.Drawing.Size(180, 22);
+            this.cntMenuEditCopy.Text = "Скопировать";
+            this.cntMenuEditCopy.Click += new System.EventHandler(this.cntMenuEditCopy_Click);
+            // 
+            // cntMenuEditPaste
+            // 
+            this.cntMenuEditPaste.Name = "cntMenuEditPaste";
+            this.cntMenuEditPaste.Size = new System.Drawing.Size(180, 22);
+            this.cntMenuEditPaste.Text = "Вставить";
+            this.cntMenuEditPaste.Click += new System.EventHandler(this.cntMenuEditPaste_Click);
+            // 
+            // cntMenuEditCut
+            // 
+            this.cntMenuEditCut.Name = "cntMenuEditCut";
+            this.cntMenuEditCut.Size = new System.Drawing.Size(180, 22);
+            this.cntMenuEditCut.Text = "Вырезать";
+            this.cntMenuEditCut.Click += new System.EventHandler(this.cntMenuEditCut_Click);
             // 
             // dialogOpen
             // 
@@ -89,56 +139,107 @@
             // menuItemFileNew
             // 
             this.menuItemFileNew.Name = "menuItemFileNew";
-            this.menuItemFileNew.Size = new System.Drawing.Size(180, 22);
+            this.menuItemFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuItemFileNew.Size = new System.Drawing.Size(163, 22);
             this.menuItemFileNew.Text = "Создать";
+            this.menuItemFileNew.Click += new System.EventHandler(this.menuItemFileNew_Click);
             // 
             // menuItemFileOpen
             // 
             this.menuItemFileOpen.Name = "menuItemFileOpen";
-            this.menuItemFileOpen.Size = new System.Drawing.Size(180, 22);
+            this.menuItemFileOpen.Size = new System.Drawing.Size(163, 22);
             this.menuItemFileOpen.Text = "Открыть";
             // 
             // menuItemFileSave
             // 
             this.menuItemFileSave.Name = "menuItemFileSave";
-            this.menuItemFileSave.Size = new System.Drawing.Size(180, 22);
+            this.menuItemFileSave.Size = new System.Drawing.Size(163, 22);
             this.menuItemFileSave.Text = "Сохранить";
             // 
             // menuItemFileSaveAs
             // 
             this.menuItemFileSaveAs.Name = "menuItemFileSaveAs";
-            this.menuItemFileSaveAs.Size = new System.Drawing.Size(180, 22);
+            this.menuItemFileSaveAs.Size = new System.Drawing.Size(163, 22);
             this.menuItemFileSaveAs.Text = "Сохранить как...";
             // 
             // menuItemFileClose
             // 
             this.menuItemFileClose.Name = "menuItemFileClose";
-            this.menuItemFileClose.Size = new System.Drawing.Size(180, 22);
+            this.menuItemFileClose.Size = new System.Drawing.Size(163, 22);
             this.menuItemFileClose.Text = "Закрыть";
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(160, 6);
             // 
             // menuItemAppExit
             // 
             this.menuItemAppExit.Name = "menuItemAppExit";
-            this.menuItemAppExit.Size = new System.Drawing.Size(180, 22);
+            this.menuItemAppExit.Size = new System.Drawing.Size(163, 22);
             this.menuItemAppExit.Text = "Выход";
+            // 
+            // toolMain
+            // 
+            this.toolMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolMain.Location = new System.Drawing.Point(0, 24);
+            this.toolMain.Name = "toolMain";
+            this.toolMain.Size = new System.Drawing.Size(791, 25);
+            this.toolMain.TabIndex = 3;
+            this.toolMain.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(70, 22);
+            this.toolStripButton1.Text = "Создать";
+            this.toolStripButton1.Click += new System.EventHandler(this.menuItemFileNew_Click);
+            // 
+            // statusMain
+            // 
+            this.statusMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stLabelRowTitle,
+            this.stLabelRow});
+            this.statusMain.Location = new System.Drawing.Point(0, 567);
+            this.statusMain.Name = "statusMain";
+            this.statusMain.Size = new System.Drawing.Size(791, 22);
+            this.statusMain.TabIndex = 4;
+            this.statusMain.Text = "statusStrip1";
+            // 
+            // stLabelRowTitle
+            // 
+            this.stLabelRowTitle.Name = "stLabelRowTitle";
+            this.stLabelRowTitle.Size = new System.Drawing.Size(46, 17);
+            this.stLabelRowTitle.Text = "Строка";
+            // 
+            // stLabelRow
+            // 
+            this.stLabelRow.Name = "stLabelRow";
+            this.stLabelRow.Size = new System.Drawing.Size(19, 17);
+            this.stLabelRow.Text = "10";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 645);
+            this.ClientSize = new System.Drawing.Size(791, 589);
+            this.Controls.Add(this.statusMain);
+            this.Controls.Add(this.toolMain);
             this.Controls.Add(this.txtEditBox);
             this.Controls.Add(this.menuMain);
             this.MainMenuStrip = this.menuMain;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.cntMenuEditBox.ResumeLayout(false);
             this.menuMain.ResumeLayout(false);
             this.menuMain.PerformLayout();
+            this.toolMain.ResumeLayout(false);
+            this.toolMain.PerformLayout();
+            this.statusMain.ResumeLayout(false);
+            this.statusMain.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,5 +260,14 @@
         private ToolStripMenuItem menuItemFileClose;
         private ToolStripSeparator toolStripMenuItem1;
         private ToolStripMenuItem menuItemAppExit;
+        private ToolStrip toolMain;
+        private ToolStripButton toolStripButton1;
+        private StatusStrip statusMain;
+        private ToolStripStatusLabel stLabelRowTitle;
+        private ToolStripStatusLabel stLabelRow;
+        private ContextMenuStrip cntMenuEditBox;
+        private ToolStripMenuItem cntMenuEditCopy;
+        private ToolStripMenuItem cntMenuEditPaste;
+        private ToolStripMenuItem cntMenuEditCut;
     }
 }
--- WinFormsNotePadProject/Form1.cs
+++ WinFormsNotePadProject/Form1.cs
@@ -2,11 +2,25 @@ namespace WinFormsNotePadProject
 {
     public partial class Form1 : Form
     {
+        string fileName;
+        bool fileSave;
+
+        int row;
+        int col;
+
+        string buffer;
+
         public Form1()
         {
             InitializeComponent();
             DialogsSetup();
-            
+            MenuMainSetup();
+
+            fileName = "noname.txt";
+            fileSave = true;
+            row = col = 1;
+
+            stLabelRow.Text = row.ToString();
         }
 
 
@@ -25,15 +39,24 @@ namespace WinFormsNotePadProject
         {
             ToolStripMenuItem menuFontItem = new("Ўрифт");
             ToolStripMenuItem menuFontItemChange = new("»зменить");
-            ToolStripMenuItem menuFontItemColor = new("÷вет");
+            ToolStripMenuItem menuFontItemColor = new("÷вет шрифта");
             ToolStripSeparator menuFontSeparator = new ToolStripSeparator();
             ToolStripMenuItem menuFontItemBackColor = new("÷вет фона");
+
+            menuMain.Items.Add(menuFontItem);
+            menuFontItem.DropDownItems.Add(menuFontItemChange);
+            menuFontItem.DropDownItems.Add(menuFontItemColor);
+            menuFontItem.DropDownItems.Add(menuFontSeparator);
+            menuFontItem.DropDownItems.Add(menuFontItemBackColor);
+
+            menuFontItemChange.Click += menuFontItemChange_Click!;
+            menuFontItemChange.ShortcutKeys = Keys.Alt | Keys.F;
         }
 
 
 
-
-
+        /*
+        
         private void btnOpen_Click(object sender, EventArgs e)
         {
             if (dialogOpen.ShowDialog() == DialogResult.Cancel)
@@ -84,10 +107,109 @@ namespace WinFormsNotePadProject
             txtEditBox.ForeColor = dialogColor.Color;
         }
 
+        
+        */
+
+
+        private void menuItemFileNew_Click(object sender, EventArgs e)
+        {
+            // проверить есть ли сейчас в окне несохраненный текст.
+            
+            if(!fileSave)
+            {
+                string message = "—охранить в файле " + fileName + "?";
+                DialogResult result = MessageBox.Show(message, "—охраненить содержимое?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        if (dialogSave.ShowDialog() == DialogResult.Cancel)
+                            return;
+                        string fileName = dialogSave.FileName;
+                        File.WriteAllText(fileName, txtEditBox.Text);
+                        break;
+                    case DialogResult.Cancel:
+                        return;
+                }
+            }
+            
+            txtEditBox.Text = "";
+            fileName = "noname.txt";
+            fileSave = true;
+            return;
+        }
+
+
+        private void menuFontItemChange_Click(object sender, EventArgs e)
+        {
+            dialogFont.Font = txtEditBox.Font;
+            dialogFont.Color = txtEditBox.ForeColor;
+
+            if (dialogFont.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            txtEditBox.Font = dialogFont.Font;
+            txtEditBox.ForeColor = dialogFont.Color;
+        }
+
+        private void txtEditBox_TextChanged(object sender, EventArgs e)
+        {
+            fileSave = false;
+        }
+
         private void dialogFont_Apply(object sender, EventArgs e)
         {
             txtEditBox.Font = dialogFont.Font;
             txtEditBox.ForeColor = dialogFont.Color;
+        }
+
+        private void txtEditBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Keys keys = (Keys)e.KeyChar;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    row++;
+                    break;
+                case Keys.Up:
+                    if(row > 1)
+                        row--;
+                    break;
+                case Keys.Down:
+                    if(row < txtEditBox.Lines.Length)
+                        row++;
+                    break;
+                default:
+                    break;
+            }
+
+            stLabelRow.Text = row.ToString();
+        }
+
+        private void cntMenuEditCopy_Click(object sender, EventArgs e)
+        {
+            if(txtEditBox.SelectionLength > 0)
+                buffer = txtEditBox.SelectedText;
+        }
+
+        private void cntMenuEditCut_Click(object sender, EventArgs e)
+        {
+            if (txtEditBox.SelectionLength > 0)
+            {
+                string text = txtEditBox.Text;
+                buffer = txtEditBox.SelectedText;
+                text = text.Remove(txtEditBox.SelectionStart, txtEditBox.SelectionLength);
+                txtEditBox.Text = text;
+            }
+        }
+
+        private void cntMenuEditPaste_Click(object sender, EventArgs e)
+        {
+            int position = txtEditBox.SelectionStart;
+            string text = txtEditBox.Text;
+            text = text.Insert(txtEditBox.SelectionStart, buffer);
+            txtEditBox.Text = text;
+            txtEditBox.SelectionStart = position + buffer.Length;
         }
     }
 }
